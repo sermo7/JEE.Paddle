9e0833e3d5dffa90ff4b4c0e17b2597f9a7b92c9
jbernal <jbernal@etsisi.upm.es>
2016-01-29 15:20:47 +0100

    Reserve. OK

--- src/main/java/business/aspect/Logs.java
+++ src/main/java/business/aspect/Logs.java
@@ -1,9 +1,13 @@
 package business.aspect;
 
+import java.util.Calendar;
+
 import org.apache.logging.log4j.LogManager;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -37,6 +41,16 @@ public class Logs {
     public void consejoJ(JoinPoint jp, Exception exception) {
         String log = "<<< " + jp.getSignature().getName() + ": Exception: " + exception.getClass().getSimpleName();
         LogManager.getLogger(jp.getSignature().getDeclaringTypeName()).info(log);
+    }
+
+    @Around("allResources()")
+    public Object consejoE(ProceedingJoinPoint pjp) throws Throwable {
+        Calendar before = Calendar.getInstance();
+        Object obj = pjp.proceed();
+        Calendar now = Calendar.getInstance();
+        LogManager.getLogger(pjp.getSignature().getDeclaringTypeName()).info(
+                "Processing time: " + (now.getTimeInMillis() - before.getTimeInMillis()) + "ms");
+        return obj;
     }
 
 }
--- src/test/java/api/ReserveResourceFunctionalTesting.java
+++ src/test/java/api/ReserveResourceFunctionalTesting.java
@@ -19,11 +19,13 @@ public class ReserveResourceFunctionalTesting {
         restService.createCourt("1");
         restService.createCourt("2");
         String token = restService.registerAndLoginPlayer();
-        Calendar day = new GregorianCalendar(2016, 00, 29, 12, 0);
+        Calendar day = Calendar.getInstance();
+        day.add(Calendar.DAY_OF_YEAR, 1);
+        day.set(Calendar.HOUR_OF_DAY, 12);
         new RestBuilder<String>(RestService.URL).path(Uris.RESERVES).basicAuth(token, "").body(new AvailableTime(1, day)).post().build();
-        day = new GregorianCalendar(2016, 00, 29, 14, 0);
+        day.set(Calendar.HOUR_OF_DAY, 14);
         new RestBuilder<String>(RestService.URL).path(Uris.RESERVES).basicAuth(token, "").body(new AvailableTime(2, day)).post().build();
-        String day2 = "" + new GregorianCalendar(2016, 00, 29).getTimeInMillis();
+        String day2 = "" + day.getTimeInMillis();
         String response = new RestBuilder<String>(RestService.URL).path(Uris.RESERVES).path(Uris.AVAILABILITY).basicAuth(token, "")
                 .param("day", day2).clazz(String.class).get().build();
         LogManager.getLogger(this.getClass()).info("testshowAvailability (" + response + ")");
