7687be3ada3c402fcfb399bed9351c571545e448
jbernal <jbernal@etsisi.upm.es>
2016-03-03 13:50:17 +0100

    Refactoring admin in properties

--- src/main/java/data/services/Populate.java
+++ src/main/java/data/services/Populate.java
@@ -7,6 +7,7 @@ import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import config.ResourceNames;
@@ -22,6 +23,9 @@ import data.entities.User;
 public class Populate {
 
     @Autowired
+    private Environment environment;
+
+    @Autowired
     private UserDao userDao;
 
     @Autowired
@@ -29,13 +33,13 @@ public class Populate {
 
     @PostConstruct
     public void createDefaultAdmin() {
-        User admin = new User("admin", "admin@gmail.com", "admin", new GregorianCalendar(1979, 07, 22));
+        User admin = new User(environment.getProperty("admin.username"), environment.getProperty("admin.email"),
+                environment.getProperty("admin.password"), new GregorianCalendar(1979, 07, 22));
         User adminSaved = userDao.findByUsernameOrEmail(admin.getUsername());
         if (adminSaved == null) {
             userDao.save(admin);
             authorizationDao.save(new Authorization(admin, Role.ADMIN));
         }
     }
-    
-    
+
 }
