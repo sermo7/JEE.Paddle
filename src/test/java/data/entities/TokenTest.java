d64e44c26e6fe777d9581a4bd0dc74cb65138b7c
Sergio Morato <s.mvillar@alumnos.upm.es>
2016-03-23 19:19:23 +0100

    Añadido test si token caducado funciona correctamente.

--- src/test/java/data/entities/TokenTest.java
+++ src/test/java/data/entities/TokenTest.java
@@ -17,5 +17,17 @@ public class TokenTest {
         Token token = new Token(user);
         assertTrue(token.getValue().length() > 20);
     }
+    
+    @Test
+    public void testhasExpired() {
+    	Calendar dateToTest = Calendar.getInstance();
+    	User user = new User("u", "u@gmail.com", "p", dateToTest);
+    	Token token = new Token(user);
+    	assertTrue(!token.hasExpired());
+    	Token tokenExpired = new Token(user);
+    	dateToTest.add(Calendar.MINUTE, -60);
+    	tokenExpired.setCreationDate(dateToTest);
+    	assertTrue(tokenExpired.hasExpired());
+    }
 
 }
