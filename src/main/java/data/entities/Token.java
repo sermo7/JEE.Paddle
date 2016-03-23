a7e6fcd5286398e68f125309b4489a3342d84d71
Sergio Morato <s.mvillar@alumnos.upm.es>
2016-03-23 19:09:38 +0100

    Añadido atributo fechaCreación a la entidad token, con get y set, además de su generación y método para saber si está caducado.

--- src/main/java/data/entities/Token.java
+++ src/main/java/data/entities/Token.java
@@ -1,5 +1,6 @@
 package data.entities;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -12,12 +13,17 @@ import javax.persistence.ManyToOne;
 @Entity
 public class Token {
 
+	public static final long EXPIRATION_DATE = 3600000;
+	
     @Id
     @GeneratedValue
     private int id;
 
     @Column(unique = true, nullable = false)
     private String value;
+    
+    @Column (nullable = false)
+    private Calendar creationDate;
 
     @ManyToOne
     @JoinColumn
@@ -31,6 +37,7 @@ public class Token {
         this.user = user;
         this.value = new Encrypt().encryptInBase64UrlSafe("" + user.getId() + user.getUsername() + Long.toString(new Date().getTime())
                 + user.getPassword());
+        this.creationDate = Calendar.getInstance();
     }
 
     public int getId() {
@@ -45,6 +52,19 @@ public class Token {
         return user;
     }
 
+    public Calendar getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Calendar creationDate) {
+		this.creationDate = creationDate;
+	}
+	
+	public boolean hasExpired(){
+		return (Calendar.getInstance().getTimeInMillis()-this.creationDate.getTimeInMillis()> EXPIRATION_DATE);
+	}
+    
+    
     @Override
     public int hashCode() {
         return id;
@@ -66,6 +86,6 @@ public class Token {
 
     @Override
     public String toString() {
-        return "Token [id=" + id + ", value=" + value + ", userId=" + user.getId() + "]";
+        return "Token [id=" + id + ", value=" + value + ", dateCreation=" + creationDate + ", userId=" + user.getId() + "]";
     }
 }
