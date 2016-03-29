ad8a81c850badb31f0db738742708127d616bcfa
Sergio Morato <s.mvillar@alumnos.upm.es>
2016-03-29 21:23:37 +0200

    Test de training ok, pero muestra fecha mal

---
+++ src/test/java/data/entities/TrainingTest.java
@@ -0,0 +1,26 @@
+package data.entities;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Calendar;
+
+import org.junit.Test;
+
+public class TrainingTest {
+
+    @Test
+    public void testTraining() {
+        User trainer = new User("trainer", "trainer@gmail.com", "p", Calendar.getInstance());
+        Court court = new Court();
+        Calendar startDate = Calendar.getInstance();
+        Calendar finishDate = startDate;
+        Training trainingTest = new Training (court, trainer, startDate, finishDate, null);
+        finishDate.add(Calendar.MINUTE, 60);
+        trainingTest.setFinishDate(finishDate);
+        assertTrue(trainingTest.getCourt().getId()==court.getId());
+        assertTrue(trainingTest.getTrainer().getId()==trainer.getId());
+        assertTrue(trainingTest.getPupils()==null);
+        System.out.println(trainingTest.toString());
+    }
+    
+}
